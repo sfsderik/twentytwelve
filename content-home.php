f0e4b9abd2c093dfbe1fd093a16b5f1fc63b5e56
Derik Seno <vonns@surefiresocial.com>
2018-03-15 03:36:35 +0800

    homepage

--- content-home.php
+++ content-home.php
@@ -1,16 +1,16 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-        <?php if ( ! is_page_template( 'page-templates/front-page.php' ) ) : ?>
-        <?php the_post_thumbnail(); ?>
-        <?php endif; ?>
-        <h1 class="entry-title"><?php the_title(); ?></h1>
-    </header>
-
-    <div class="entry-content">
-        <?php the_content(); ?>
-        <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
-    </div><!-- .entry-content -->
-    <footer class="entry-meta">
-        <?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
-    </footer><!-- .entry-meta -->
+    <div class="row">
+        <div class="page-thumbnail">
+            <?php the_post_thumbnail(); ?>
+        </div>
+        <div class="page-content">
+            <header class="entry-header">
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+            </header>
+            <div class="entry-content">
+                <?php the_content(); ?>
+                <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
+            </div><!-- .entry-content -->
+        </div>
+    </div>
 </article><!-- #post -->
